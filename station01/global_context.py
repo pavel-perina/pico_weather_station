5938c71759e6a9cf672ae3a88a071f11c853ea2d
Pavel Perina <pavel.perina@gmail.com>
2024-12-31 11:39:05 +0100

    add early station project

---
+++ station01/global_context.py
@@ -0,0 +1,26 @@
+
+class GlobalContext:
+    """
+    Global application context which stores various sensor data, event triggers...
+    """
+    def __init__(self, i2c, spi):
+        self.i2c = i2c
+        self.spi = spi
+        self.ticks_ms = 0
+        self.sht40_temperature = 0.0
+        self.sht40_humidity = 0.0
+        self.sht40_valid = False # Not baking or cooling
+        self.sht40_mock = True
+        self.bmp280_temperature = 0.0
+        self.bmp280_pressure = 0.0
+        self.bmp280_mock = True
+        self.backlight_percents = 0.0
+        self.key_up_pressed = False
+        self.key_down_pressed = False
+        self.key_left_pressed = False
+        self.key_right_pressed = False
+        self.fb_width  = 84
+        self.fb_height = 44
+        self.framebuffer =  bytearray([0xAA] * (self.fb_width * self.fb_height // 8))
+        self.framebuffer_dirty = False
+        self.app = "SCREEN_MAIN"
---
+++ station01/main.py
@@ -0,0 +1,36 @@
+from machine import I2C, SPI, Pin
+import time
+from global_context import GlobalContext
+from sensor_sht40 import Sht40
+from display_pcd8544 import Display
+from screen_main import ScreenMain
+#from screen_menu import 
+
+ctx = GlobalContext(I2C(0, sda=Pin(0), scl=Pin(1)), 
+                    SPI(1, baudrate=1000000, mosi=Pin(11), sck=Pin(10))
+)
+display = Display(ctx)
+screen_main = ScreenMain(ctx)
+sht40 = Sht40()
+while True:
+    # Time stuff
+    time.sleep_ms(5)
+    ctx.ticks_ms = time.ticks_ms()
+
+    # Read sensors
+    sht40.on_tick(ctx)
+    #bmp280.on_tick(ctx)
+    #rtc.on_tick(ctx)
+
+    # Translate keyboard buttons to events
+    #keyboard.on_tick(ctx)
+
+    # Data logging stuff
+    # logger.on_tick(ctx)
+    if (ctx.app == "SCREEN_MAIN"):
+        screen_main.on_tick(ctx)
+        
+
+    # Update display from framebuffer if dirty
+    display.on_tick(ctx) 
+    #print(f"Ticks: {ctx.ticks_ms}, Temp: {ctx.sht40_temperature}")

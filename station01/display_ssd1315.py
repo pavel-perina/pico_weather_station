b8e46fda2c0e7aefbb0d2538d8cc83a379e55686
Pavel Perina <pavel.perina@gmail.com>
2025-10-17 17:59:47 +0200

    found working init sequence for ssd1315

--- station01/display_ssd1315.py
+++ station01/display_ssd1315.py
@@ -9,31 +9,33 @@ from global_context import GlobalContext
 import time
 
 SSD1306_ADDR = 0x3C
-
+# Note: second one works
 # https://www.flowcode.co.uk/forums/viewtopic.php?t=3020
+# https://bitbucket.org/tinusaur/ssd1306xled/src/master/ssd1306xled/ssd1306xled.c
 def display_init(i2c):
     print("ssd1315.display_init()")
     # NOTE: C0,A0 - up is cable side
     #       C8,A1 - up is pin side
     commands = [
-            0xAE, # Display OFF
-    0xD5, 0x80, # Clock
-    0xA8, 0x3F, # Multiplex ratio (64 rows)
-    0xD3, 0x00, # Offset
-    0x40, # Start line at 0
-    0x8D, 0x14, # Charge pump
-    0x20, 0x00, # Horizontal addressing mode
-    0xC0, # Scan direction
-    0xA0, # Segment re-map
-    0xDA, 0x12, # COM hardware config
-    0x81, 0x7F, # Contrast
-    0xD9, 0xF1, # Pre-charge
-    0xDB, 0x40, # VCOMH deselect
-    0xA4, # Entire display ON
-    0xA6, # Normal display
-    #0x21, 0x03, 0x82, # Column address range (shift +3)
-    0x22, 0x00, 0x07, # Page address range (0-7)
-    0xAF # Display ON
+            	0xAE, # Set Display ON/OFF - AE=OFF, AF=ON
+	0xD5, 0xF0,		# Set display clock divide ratio/oscillator frequency, set divide ratio
+	0xA8, 0x3F,		# Set multiplex ratio (1 to 64) ... (height - 1)
+	0xD3, 0x00,		# Set display offset. 00 = no offset
+	0x40 | 0x00,	# Set start line address, at 0.
+	0x8D, 0x14,		# Charge Pump Setting, 14h = Enable Charge Pump
+	0x20, 0x00,		# Set Memory Addressing Mode - 00=Horizontal, 01=Vertical, 10=Page, 11=Invalid
+	0xA0 | 0x00,	# Set Segment Re-map
+	0xC0,			# Set COM Output Scan Direction
+	0xDA, 0x12,		# Set COM Pins Hardware Configuration - 128x32:0x02, 128x64:0x12
+	0x81, 0x3F,		# Set contrast control register
+	0xD9, 0x22,		# Set pre-charge period (0x22 or 0xF1)
+	0xDB, 0x20,		# Set Vcomh Deselect Level - 0x00: 0.65 x VCC, 0x20: 0.77 x VCC (RESET), 0x30: 0.83 x VCC
+	0xA4,			# Entire Display ON (resume) - output RAM to display
+	0xA6,			# Set Normal/Inverse Display mode. A6=Normal; A7=Inverse
+	0x2E,			# Deactivate Scroll command
+	0x22, 0x00, 0x3f,	# Set Page Address (start,end)
+	0x21, 0x00,	0x7f,	# Set Column Address (start,end)
+	0xAF,			# Set Display ON/OFF - AE=OFF, AF=ON
     ]
 
     for cmd in commands:
@@ -53,7 +55,7 @@ class Display:
     def on_tick(self, ctx):
         if ctx.framebuffer_dirty:
             reset_cursor(self.i2c)
-            chunk_size = 1024 #32
+            chunk_size = 32 # Note: even 1024 works
             for i in range(0, len(ctx.framebuffer), chunk_size):
                 chunk = ctx.framebuffer[i:i+chunk_size]
                 self.i2c.writeto(SSD1306_ADDR, bytes([0x40]) + chunk)
